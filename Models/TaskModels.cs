7872e810b772e696ab5c31617ed61b3e58fb8243
Andoni <andoni.fernandezlo@elorrieta-errkamari.com>
2023-10-18 08:25:49 +0200

    Agregar archivos de proyecto.

---
+++ Models/TaskModels.cs
@@ -0,0 +1,9 @@
+﻿namespace Tasky.Models
+{
+    public class TaskModels
+    {
+        public string TaskName { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+    }
+}
